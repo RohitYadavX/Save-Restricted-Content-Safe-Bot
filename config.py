b497b382947d79a38db76c48dde3cddd4a802698
RohitYadavX <viraleditorofficial@gmail.com>
2024-10-11 21:22:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "24935975"))
+API_HASH = getenv("API_HASH", "0f628b53aba7debe2de4b24dd85c1a49")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7593890865").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://officialstoryhindi:<BPl66X2Rghdbvyti>@cluster0.gmdzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
