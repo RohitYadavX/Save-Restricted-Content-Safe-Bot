2f1fa452316feed09f0d9d3198248e001e24c705
RohitYadavX <viraleditorofficial@gmail.com>
2024-10-11 22:46:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "24935975"))
-API_HASH = getenv("API_HASH", "0f628b53aba7debe2de4b24dd85c1a49")
+API_ID = int(getenv("API_ID", "24975"))
+API_HASH = getenv("API_HASH", "0f53aba7debe2de4b24dd85c1a49")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7593890865").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://officialstoryhindi:<BPl66X2Rghdbvyti>@cluster0.gmdzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-LOG_GROUP = getenv("LOG_GROUP", "-1002295215916")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002388870358"))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://officialstoryhindi:<66X2Rghvyti>@cluster0.gmdzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-100229521516"
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-10023888358"))
